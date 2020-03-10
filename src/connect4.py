20954dcee0048f5ea9b5181120711bab621d2966
Aurelien Simon <aureliensimon29260@gmail.com>
2020-03-10 19:42:33 +0100

    + comments

--- src/connect4.py
+++ src/connect4.py
@@ -5,9 +5,11 @@ from player import Player
 p1 = Player('human', 'x')
 p2 = Player('zer0lose', 'o')
 
+# Initialisation of the game board with empty char
 def initBoard ():
     return [[' '] * 7 for i in range(7)]
 
+# Print the current board
 def printBoard (board):
     os.system('clear')
     for i in range(7):
@@ -15,28 +17,33 @@ def printBoard (board):
         print (' ---+---+---+---+---+---+---')
     print ('  1   2   3   4   5   6   7')
 
+# Get the index of the first empty space in a colown
 def getFreeSpace (board, col):
     for i in range(7):
         if (board[col][i] == ' '):
             return i
     return None
 
+# Put the player's tag in the board at one spot
 def fillBoard (board, col, index, tag):
     board[col][index] = tag
     return board
 
+# Check if a row is a winning one
 def checkRow (board, Row, tag):
     for i in range(4):
         if (board[i][Row] == board[i + 1][Row] == board[i + 2][Row] == board[i + 3][Row]) and (board[i][Row] == tag):
             return True
     return False
 
+# Check if a colomn is a winning one
 def checkColumn (board, col, tag):
     for i in range(4):
         if (board[col][i] == board[col][i + 1] == board[col][i + 2] == board[col][i + 3]) and (board[col][i] == tag):
             return True
     return False
 
+# Check if one of the diagonal is a winning one
 def checkDiagonal (board, tag):
     for i in range(4):
         for j in range(4):
@@ -44,17 +51,20 @@ def checkDiagonal (board, tag):
                 return True
     return False
 
+# Check if game is over
 def gameOver (board, player):
     for i in range(7):
         if checkRow(board, i, player.tag) or checkColumn(board, i, player.tag): return True
     return checkDiagonal(board, player.tag)
 
+# Check if it's a draw (no more space)
 def gameDraw (board):
     for i in range(7):
         for j in range(7):
             if (board[i][j] == ' '): return False
     return True
 
+# Start Connect 4 game
 def connect4 ():
     board = initBoard()
     printBoard(board)
@@ -64,7 +74,7 @@ def connect4 ():
     turn = 1
 
     while True:
-            
+
         if (turn):
             while True:
                 choice = int(input())
